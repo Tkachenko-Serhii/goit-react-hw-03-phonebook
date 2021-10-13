9c8994a56ef297d1618de46c41ed0203da3f8a30
Serhii Tkachenko <79119130+Tkachenko-Serhii@users.noreply.github.com>
2021-10-13 22:29:14 +0300

    add save to local storage

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import shortId from "shortid";
 
 export default class App extends React.Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   getFormData = ({ name, number }) => {
     if (this.state.contacts.find((contact) => contact.name === name)) {
